7da3151db209245e09baefa605b49ea898bda949
Ben-isLearning <68328490+Ben-isLearning@users.noreply.github.com>
2022-03-21 10:39:44 +0000

    Initial Commit

---
+++ main.js
@@ -0,0 +1,40 @@
+let personArray = [
+  "Brad Pitt",
+  "Christian Bale",
+  "Charlie Chaplin",
+  "Hugo Weaving",
+  "Micheal Caine",
+  "Tom Hanks",
+  "Leonardo DiCaprio",
+  "Morgan Freeman",
+  "Samuel L. Jackson",
+  "Robert De Niro",
+];
+
+let actionArray = [
+  "laugh at",
+  "dance on",
+  "jump through",
+  "kick the",
+  "lie to",
+  "drink the",
+  "eat some",
+  "paint a",
+  "draw the",
+  "ravage some",
+];
+
+let thingArray = [
+  "children",
+  "lamp",
+  "cat",
+  "bridge",
+  "oscar",
+  "flowers",
+  "animal",
+  "sasquach",
+  "beavers",
+  "toast",
+];
+
+// output "Why did the X, Y the Z?"
